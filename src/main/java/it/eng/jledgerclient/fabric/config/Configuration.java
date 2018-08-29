21da5349c2ef69eb96293726dbc70a3a553dab75
Antonio Scatoloni <antonio.scatoloni@gmail.com>
2018-08-29 12:28:29 +0200

    New version of certificates management in client application.

--- src/main/java/it/eng/jledgerclient/fabric/config/Configuration.java
+++ src/main/java/it/eng/jledgerclient/fabric/config/Configuration.java
@@ -66,7 +66,7 @@ public class Configuration {
 
     public String getCryptoconfigdir() {
         if (StringUtils.isEmpty(cryptoconfigdir))
-            this.setCryptoconfigdir(System.getProperty("user.home") + "/crypto-config");
+            this.setCryptoconfigdir(System.getProperty("user.home") + "/crypto-users");
         return cryptoconfigdir;
     }
 
